e05fde82be69c006ff87f39bfe3ab334944073f1
scetin23 <sultancetin2022@gmail.com>
2023-11-13 15:33:33 +0300

    Repository ekledim , uygun olduğunuzda bakalım doğruysa Menü ve EkMalzemeler için de repository oluştururum

---
+++ Hamburgerci.Repository/Abstract/ISiparisRepository.cs
@@ -0,0 +1,26 @@
+﻿using Hamburgerci.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamburgerci.Repository.Abstract
+{
+    public interface ISiparisRepository
+    {
+        List<Siparis> GetAllSiparisler();
+
+        Siparis GetSiparisById(int id);
+
+        bool AddSiparis(Siparis siparis);
+
+        bool UpdateSiparis(Siparis siparis);
+
+        bool DeleteSiparis(int id);
+
+        int Save();
+
+
+    }
+}
