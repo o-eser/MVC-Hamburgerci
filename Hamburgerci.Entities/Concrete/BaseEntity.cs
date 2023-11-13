c38aa6db7edf03186f8d4cdb045a54b35ca0fb5b
Osman eser <osmaneser513@live.com>
2023-11-13 22:59:20 +0300

    kullanıcı eklendi ve entitiy lere baseentitiy den miras verildi

---
+++ Hamburgerci.Entities/Concrete/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hamburgerci.Entities.Abstract;
+
+namespace Hamburgerci.Entities.Concrete
+{
+    public class BaseEntity : IBaseEntity
+    {
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public Kullanici CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Kullanici ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public DateTime DeletedDate { get; set; }
+        public Kullanici DeletedBy { get; set; }
+    }
+}
---
+++ Hamburgerci.Entities/Concrete/Kullanici.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hamburgerci.Entities.Abstract;
+using Microsoft.AspNetCore.Identity;
+
+namespace Hamburgerci.Entities.Concrete
+{
+    public class Kullanici : IdentityUser<Guid>
+    {
+        public int Id { get ; set; }
+        public Kullanici CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Kullanici ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public Kullanici DeletedBy { get; set; }
+    }
+}
