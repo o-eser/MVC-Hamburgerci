fd99155f996e1def58c7a5933717ee7f00cdd757
Can TOKHAY <cantokhay@outlook.com>
2023-11-15 21:22:24 +0300

    commit stage can

--- Hamburgerci.Repositories/Concrete/KullaniciRepository.cs
+++ Hamburgerci.Repositories/Concrete/KullaniciRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Hamburgerci.Entities.Concrete;
+﻿using Hamburgerci.Entities.Concrete;
 using Hamburgerci.Repositories.Abstract;
 using Hamburgerci.Repositories.Context;
 
