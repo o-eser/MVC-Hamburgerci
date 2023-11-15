6517b6fe2cde6e336190a9c161efc70537248df7
Can TOKHAY <cantokhay@outlook.com>
2023-11-15 14:32:28 +0300

    OneNote'daki ilerlemeyi uyguladım.

--- Hamburgerci.Entities/Concrete/Siparis.cs
+++ Hamburgerci.Entities/Concrete/Siparis.cs
@@ -1,17 +1,21 @@
-﻿using Hamburgerci.Entities.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Hamburgerci.Entities.Abstract;
+using Hamburgerci.Entities.Enum;
 
 namespace Hamburgerci.Entities.Concrete
 {
-    public class Siparis : BaseEntity
+    public class Siparis : IBaseEntity, IEntity<Guid>
     {
+        public Guid Id { get; set; }
         public MenuBoyutu MenuBoyutu { get; set; }
         public int SiparisAdeti { get; set; }
         public double ToplamTutar { get; }
+        public Kullanici CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Kullanici? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public Kullanici? DeletedBy { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public DataStatus DataStatus { get; set; }
 
         public ICollection<Menu> Menuler { get; set; }
 
---
+++ Hamburgerci.repositories/Concrete/KullaniciRepository.cs
@@ -0,0 +1,13 @@
+﻿using Hamburgerci.Entities.Concrete;
+using Hamburgerci.Repositories.Abstract;
+using Hamburgerci.Repositories.Context;
+
+namespace Hamburgerci.Repositories.Concrete
+{
+    public class KullaniciRepository : BaseRepository<Kullanici>, IKullaniciRepository
+    {
+        public KullaniciRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
