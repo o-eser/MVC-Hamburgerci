9c329372842790decf692cfc7030870a031352e9
Srykmt <142337234+Srykmt@users.noreply.github.com>
2023-11-15 21:22:24 +0300

    Commit stage

--- Hamburgerci.Repositories/Concrete/MenuRepository.cs
+++ Hamburgerci.Repositories/Concrete/MenuRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Hamburgerci.Entities.Concrete;
 using Hamburgerci.Repositories.Abstract;
 using Hamburgerci.Repositories.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hamburgerci.Repositories.Concrete
 {
