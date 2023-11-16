c538f24310490491223af9f44bc467d971ef4f45
scetin23 <sultancetin2022@gmail.com>
2023-11-16 12:30:27 +0300

    Repolar eklendi

---
+++ Hamburgerci.Repositories/Concrete/SiparisRepository.cs
@@ -0,0 +1,18 @@
+﻿using Hamburgerci.Entities.Concrete;
+using Hamburgerci.Repositories.Abstract;
+using Hamburgerci.Repositories.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamburgerci.Repositories.Concrete
+{
+    public class SiparisRepository : BaseRepository<Siparis>, ISiparisRepository
+    {
+        public SiparisRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
