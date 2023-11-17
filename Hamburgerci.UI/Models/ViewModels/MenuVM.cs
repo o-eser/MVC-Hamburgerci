2e9504b343485f4433a1862ddc4fbbf97002330e
Osman eser <osmaneser513@live.com>
2023-11-17 13:44:29 +0300

    yönetimpaneli için tablo düzenlendi pagination ve search eklendi. menü ekleme component ile menuindex eklendi

--- Hamburgerci.UI/Models/ViewModels/MenuVM.cs
+++ Hamburgerci.UI/Models/ViewModels/MenuVM.cs
@@ -1,9 +1,15 @@
 ﻿using Hamburgerci.Entities.Concrete;
+using X.PagedList;
 
 namespace Hamburgerci.UI.Models.ViewModels
 {
     public class MenuVM
     {
+        public MenuVM()
+        {
+            Menuler = new PagedList<Menu>(null, 1, 1);
+        }
         public Menu Menu { get; set; }
+        public IPagedList<Menu> Menuler { get; set; }
     }
 }
---
+++ Hamburgerci.UI/ViewComponents/MenuEkle.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hamburgerci.UI.ViewComponents
+{
+	public class MenuEkle: ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
