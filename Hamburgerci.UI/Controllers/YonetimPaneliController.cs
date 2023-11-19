22d2148d8928cdf8d27c7b26d45adfc738d9b4d1
Osman eser <osmaneser513@live.com>
2023-11-19 16:55:21 +0300

    Menü crud işlemleri tamamlandı

--- Hamburgerci.UI/Controllers/YonetimPaneliController.cs
+++ Hamburgerci.UI/Controllers/YonetimPaneliController.cs
@@ -81,45 +81,45 @@ namespace Hamburgerci.UI.Controllers
         //}
 
        
-        public IActionResult EkstraMalzemeEkle()
-        {
-            return View();
-        }
+        //public IActionResult EkstraMalzemeEkle()
+        //{
+        //    return View();
+        //}
 
-        [HttpPost]
-        public async Task<IActionResult> EkstraMalzemeEkle(EkstraMalzemeVM model)
-        {
-            if (ModelState.IsValid) 
-            {
-                await _ekstraMalzemeService.CreateAsync(model.EkstraMalzeme);
-                return RedirectToAction("Index");
-            }
-            return View(model);
-        }
+        //[HttpPost]
+        //public async Task<IActionResult> EkstraMalzemeEkle(EkstraMalzemeVM model)
+        //{
+        //    if (ModelState.IsValid) 
+        //    {
+        //        await _ekstraMalzemeService.CreateAsync(model.EkstraMalzeme);
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(model);
+        //}
 
-        public async Task<IActionResult> EkstraMalzemeDuzenle(int id)
-        {
-            EkstraMalzemeVM vm = new EkstraMalzemeVM();
-            vm.EkstraMalzeme = await _ekstraMalzemeService.GetByIdAsync(id);
-            return View(vm);
-        }
+        //public async Task<IActionResult> EkstraMalzemeDuzenle(int id)
+        //{
+        //    EkstraMalzemeVM vm = new EkstraMalzemeVM();
+        //    vm.EkstraMalzeme = await _ekstraMalzemeService.GetByIdAsync(id);
+        //    return View(vm);
+        //}
 
-        [HttpPost]
-        public IActionResult EkstraMalzemeDuzenle(EkstraMalzemeVM model)
-        {
-            if (ModelState.IsValid)
-            {
-                _ekstraMalzemeService.Update(model.EkstraMalzeme);
-                return RedirectToAction("Index");
-            }
-            return View(model);
-        }
+        //[HttpPost]
+        //public IActionResult EkstraMalzemeDuzenle(EkstraMalzemeVM model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _ekstraMalzemeService.Update(model.EkstraMalzeme);
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(model);
+        //}
 
-        public async Task<IActionResult> EkstraMalzemeSil(int id)
-        {
-            await _ekstraMalzemeService.RemoveAsync(id);
-            return RedirectToAction("Index");
-        }
+        //public async Task<IActionResult> EkstraMalzemeSil(int id)
+        //{
+        //    await _ekstraMalzemeService.RemoveAsync(id);
+        //    return RedirectToAction("Index");
+        //}
 
 
     }
---
+++ Hamburgerci.UI/ViewComponents/MenuDuzenle.cs
@@ -0,0 +1,14 @@
+﻿using Hamburgerci.Application.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hamburgerci.UI.ViewComponents
+{
+	public class MenuDuzenle: ViewComponent
+	{
+		public IViewComponentResult Invoke(MenuDTO model)
+		{
+			MenuDTO menu = model;
+			return View(menu);
+		}
+	}
+}
