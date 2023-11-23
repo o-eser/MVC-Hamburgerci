2dca75b46aee8c71f77ea99cc792268e8aa97161
Srykmt <142337234+Srykmt@users.noreply.github.com>
2023-11-23 19:12:46 +0300

    Rol ile ilgili control, dto ve vm'ler eklendi. Componentler eklenecek.

---
+++ Hamburgerci.UI/ViewComponents/RolComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hamburgerci.UI.ViewComponents
+{
+    public class RolComponent : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
