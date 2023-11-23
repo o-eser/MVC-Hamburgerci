e3ae7c70d0d39d22f5a0196ce730ba1e77796c0f
Srykmt <142337234+Srykmt@users.noreply.github.com>
2023-11-23 19:12:46 +0300

    Rol ile ilgili control, dto ve vm'ler eklendi. Componentler eklenecek.

---
+++ Hamburgerci.UI/Models/ViewModels/LoginVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Hamburgerci.UI.Models.ViewModels
+{
+    public class LoginVM
+    {
+        public string ReturnUrl { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Hamburgerci.UI/Models/ViewModels/RegisterVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hamburgerci.UI.Models.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "Kullanıcı Adı Boş Geçilemez!")]
+        [Display(Name = "Kullanıcı Adı")]
+        [MinLength(3, ErrorMessage = "Kullanıcı Adı 3 Karakterden Az Olamaz!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Şifre boş geçilemez!")]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Email boş geçilemez!")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Mail Formatı Doğrulanamadı!")]
+        public string Email { get; set; }
+    }
+}
